408c0befb8938b4177a7415e5d3b31f4b5e8503e
Kevin Mojek <k.mojek@techsmith.com>
2018-10-16 13:02:12 -0400

    Some clean-up based on PR feedback; update version.

--- include/AVI20/Read/IStream.h
+++ include/AVI20/Read/IStream.h
@@ -7,17 +7,6 @@
 
 #include <stdint.h>
 
-NAMESPACE_AVI20_BEGIN
-struct AVISTDINDEX;
-struct AVISTDINDEX_ENTRY;
-struct AVISUPERINDEX;
-struct AVISUPERINDEXENTRY;
-struct BITMAPINFOHEADER;
-struct MainHeader;
-struct MediaStreamHeader;
-struct WAVEFORMATEX;
-NAMESPACE_AVI20_END
-
 NAMESPACE_AVI20_READ_BEGIN
 
 class IStream
@@ -25,18 +14,9 @@ class IStream
 public:
    virtual bool IsNULL() const = 0;
 
-   //template<class T> virtual T Read() = 0;
-   virtual void Read( uint32_t& d ) = 0;
-   virtual void Read( AVI20::AVISTDINDEX& d ) = 0;
-   virtual void Read( AVI20::AVISTDINDEX_ENTRY& d ) = 0;
-   virtual void Read( AVI20::AVISUPERINDEX& d ) = 0;
-   virtual void Read( AVI20::AVISUPERINDEXENTRY& d ) = 0;
-   virtual void Read( AVI20::BITMAPINFOHEADER& d ) = 0;
-   virtual void Read( AVI20::MainHeader& d ) = 0;
-   virtual void Read( AVI20::MediaStreamHeader& d ) = 0;
-   virtual void Read( AVI20::WAVEFORMATEX& d ) = 0;
-
+   template<typename T> void Read( T& t ) { Read( (uint8_t *)&t, sizeof( T ) ); }
    virtual bool Read( uint8_t* dest, uint64_t size ) = 0;
+
    virtual uint64_t Pos() = 0;
    virtual void SetPos( uint64_t pos ) = 0;
    virtual void SetPosToEnd() = 0;
--- include/AVI20/Read/WindowsStream.h
+++ include/AVI20/Read/WindowsStream.h
@@ -14,16 +14,6 @@ public:
 
    bool IsNULL() const override;
 
-   void Read( uint32_t& result ) override { result = read<uint32_t>(); }
-   void Read( AVI20::AVISTDINDEX& result ) override { result = read <AVI20::AVISTDINDEX>(); }
-   void Read( AVI20::AVISTDINDEX_ENTRY& result ) override { result = read<AVI20::AVISTDINDEX_ENTRY>(); }
-   void Read( AVI20::AVISUPERINDEX& result ) override { result = read<AVI20::AVISUPERINDEX>(); }
-   void Read( AVI20::AVISUPERINDEXENTRY& result ) override { result = read<AVI20::AVISUPERINDEXENTRY>(); }
-   void Read( AVI20::BITMAPINFOHEADER& result ) override { result = read<AVI20::BITMAPINFOHEADER>(); }
-   void Read( AVI20::MainHeader& result ) override { result = read<AVI20::MainHeader>(); }
-   void Read( AVI20::MediaStreamHeader& result ) override { result = read<AVI20::MediaStreamHeader>(); }
-   void Read( AVI20::WAVEFORMATEX& result ) override { result = read<AVI20::WAVEFORMATEX>(); }
-
    bool Read( uint8_t* dest, uint64_t size ) override;
    uint64_t Pos() override;
    void SetPos( uint64_t pos ) override;
